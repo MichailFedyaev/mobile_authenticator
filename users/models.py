989b7927fd3c8bb49d18abf06d57402209f2faa4
Michail Fedyaev <drovosekov1910@mail.ru>
2025-03-10 14:49:28 +0500

    вью + прочее

--- users/models.py
+++ users/models.py
@@ -14,6 +14,9 @@ class User(AbstractUser):
     USERNAME_FIELD = "phone"
     REQUIRED_FIELDS = []
 
+    def __str__(self):
+        return self.phone
+
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
