c5068fa3d202fa839524e82e0cbb87442fc78235
Michail Fedyaev <drovosekov1910@mail.ru>
2025-03-10 18:53:33 +0500

    htmlki + стили + обновлённые вью и некоторые правки

--- authapp/views.py
+++ authapp/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class IndexView(LoginRequiredMixin, TemplateView):
+    template_name = "authapp/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
