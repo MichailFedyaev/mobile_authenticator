25637a6da513e11b737c17a91fb52f3a89b7d7a8
Michail Fedyaev <drovosekov1910@mail.ru>
2025-03-25 21:51:18 +0500

    линтеры

--- authapp/forms.py
+++ authapp/forms.py
@@ -5,8 +5,10 @@ class InviteCodeForm(forms.Form):
     invite_code = forms.CharField(
         max_length=6,
         label="Инвайт-код",
-        widget=forms.TextInput(attrs={
-            'class': 'form-control form-control-lg',
-            'placeholder': 'Введите код'
-        })
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control form-control-lg",
+                "placeholder": "Введите код",
+            }
+        ),
     )
